5538aac3f9e81917a3b038691d028e606eca1709
rvillalta <rvillalta2002@yahoo.com.mx>
2016-08-02 00:27:40 -0400

    Add files via upload

--- lee_function.cpp
+++ lee_function.cpp
@@ -1,8 +1,8 @@
 /*@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-@												 @
-@		PROGRAMA QUE LEE LOS ARCHIVOS W2CAD CON LOS QUE SE ALIMENTA EL ECLIPSE           @
-@				Raul Villalta Julio 2016					 @
-@												 @
+@												 												 @
+@		PROGRAMA QUE LEE LOS ARCHIVOS W2CAD CON LOS QUE SE ALIMENTA EL ECLIPSE           		 @
+@				Raul Villalta Julio 2016					 									 @
+@												 												 @
 @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*/
 
 // COMPILAR USANDO LA OPCION: 	-STD=C++11
@@ -12,22 +12,27 @@
 #include <iostream>
 using namespace std;
 void lee_archivo(string, double * , int *);
+void escribe_archivo(string, double *, int *, int *);
 int main()
 {
 	double *dat, data[80000];
-	int *fils, control[100],  filas[100], i, j, ndatos, ntot,  k = 0, s = 1, tt;
+	string filename;
+	int *fils, *sel, control[100],  filas[100], i, j, ndatos, ntot,  k = 0, s = 1, tt, select[4] = {0, 1, 0, 1};
+	sel = select;
 	dat = data;
 	fils = filas;
 	lee_archivo("X06 OPEN 2X2 CR PinPoint.CD2", dat, fils);
+	//cout << "Entre el nombre del archivo de salida  " ;
+	//getline(cin, filename);
+	dat = data;
+	escribe_archivo("salida.txt", dat, fils, sel);
 	ndatos = filas[0];
 	control[0] = 0;
 	ntot = 0;
-	for (i = 1; i < ndatos + 1; i++){
-		ntot = ntot + filas[i];
-		cout << "filas[" << i << "]=" << filas[i] << endl;
-		control[i] = 4 * filas[i]+ control[i - 1];
-		cout << "control[" << i << "]=" << control[i] << endl;
-	}
+	/*			VER SALIDA DE DOATOS LEIDA
+	 *
+	 * 
+
 
 	cout << "######################## RESULTADOS #############################" << endl;
 	cout << "ntot " << ntot << endl;
@@ -44,10 +49,86 @@ int main()
 			cout << endl;
 			cout << "-------------------------------------------------------------------------" << endl;
 			s++;
-			cin >> tt;
+			//cin >> tt;
+		}
+		
+	}*/
+
+	return 0;
+}
+void escribe_archivo(string filename, double *datos, int *nfils, int *sel)
+{
+	/* Este programa escribe los datos leidos por la funcion
+	 * lee_archivo() y los ecribe de manera selectiva en columnas ordenadas 
+	 * de manera que puedan ser utilizadas por el programa GNUPLOT
+	 
+	 FILENAME nombre del archivo donde sera escrita la data.
+	 *DATOS puntero al vector double donde esta lamacenados los datos
+			leidos por lee_archivo()
+	 *NFILS puntero al vector int donde esta guardada la informacion
+	 		sobre el numero de filas de cada uno de los set de datos del
+	        archivo, el primer elemento (nfils[0]) almacenara eñ numero 
+	        de sets diferentes de datos
+	 */
+	 
+	 ofstream pa, pb;
+	 int s = 1, i, k, j, d, nset, col, fil, depth, cont, nfil_max = 0, select[4], ntot, control[300];
+	 double data[300][4][100];
+	 bool check;
+	 pa.open(filename);
+	 pb.open("data.txt");
+	 control[0] = 0;
+	 cont = 0;
+	 depth = 0;
+	 nset = *nfils;
+	 cout << "nset " << nset << endl;
+	 int filas[nset];
+	 nfils++;
+	 for (i = 0; i < nset +	 1; i++){filas[i] = *nfils; cout << i << "  " << filas[i] << endl; nfils++; }
+	
+	 for (i = 0; i < 4; i++){select[i] = *sel;sel++;}
+	 
+	 for (i = 0; i < nset; i++){ntot = ntot + filas[i];}
+
+	 for (i = 1; i < nset + 1 ; i++) if (nfil_max < filas[i]) nfil_max = filas[i];
+	cout << "max_nfils " << nfil_max;
+	// "******************** CREACION DE LA MATRIZ DATA[][][] *************************" << endl;
+	cont = -1;
+	for (k = 0; k < nset; k++){
+		cont++;
+		for (i = 0; i < filas[cont]; i++){
+			for (j = 0; j < 4; j++){
+				data[i][j][k] = *datos;
+				datos++;
+			}
 		}
 	}
-	return 0;
+	i = 2;
+	cout << "	my data   " << data[0][i][0] << endl;
+	cout << "	my data   " << data[0][i][1] << endl;
+	cout << "	my data   " << data[0][i][2] << endl;
+	cout << "	my data   " << data[0][i][3] << endl;
+	// "******************** FIN CREACION DE LA MATRIZ DATA[][][] *************************" << endl;
+	for (fil=0; fil < 10; fil++){
+		for(col=0; col < 4; col++){
+			cout << data[fil][col][3] << " ";
+		}
+		cout << endl;
+	}
+	cout << "*********************************************************** " << endl;
+	for (i =0; i < nfil_max; i++){
+		for (d = 0; d < nset; d++){
+			for (j = 0; j < 4; j++){
+				if (select[j]){
+					if (i > filas[d]) pa << "	" << " " ;
+					else pa << data[i][j][d] << "	";
+				}
+				 
+			 }
+		}
+		pa << endl;
+	}
+	pa.close();
 }
 
 void lee_archivo(string filename, double *datos , int *nfils)
@@ -83,7 +164,7 @@ void lee_archivo(string filename, double *datos , int *nfils)
 				if(bandera) {
 					*nfils = i;
 					nfils++;
-					cout << "#####################################################" << endl;
+					//cout << "#####################################################" << endl;
 				}
 				i = 0;
 				cont++;
